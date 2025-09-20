4853241a5be876c90807a3139b45c6f578edc4c0
toba851 <modupetoba85@gmail.com>
2025-09-20 02:04:21 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,10 +2,4 @@ resource "aws_s3_bucket" "my-s3-bucket" {
   bucket_prefix = var.bucket_prefix
   tags=var.tags
 }
-resource "aws_s3_bucket_versioning" "this"{
-  bucket=aws_S3_bucket.my-s3-bucket.id
 
-  versioning configuration {
-  status=var.versioning? "Enabled" : "Suspended'"
-  }
-}
