bdc353701e09e639fe1a08ded26671f0308378a6
toba851 <modupetoba85@gmail.com>
2025-09-20 01:49:52 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "my_s3_bucket" {
   bucket_prefix = var.bucket_prefix
   tags=var.tags
-
+}
 resource "aws_s3_bucket_versioning" "this"{
   bucket=aws_S3_bucket.my_s3_bucket.id
 
