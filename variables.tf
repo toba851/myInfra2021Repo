c3448b8d0bc89ba394e286fc6401b78b09b31f22
toba851 <modupetoba85@gmail.com>
2025-09-20 00:20:01 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "us-east-1" 
+       default     = "us-east-2" 
 }
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "mySep22Key" 
+    default     =  "myjenkinskey" 
 }
 
 variable "instance_type" { 
     description = "instance type for ec2" 
-    default     =  "t2.micro" 
+    default     =  "t3.micro" 
 }
 
 variable "security_group" { 
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0ee23bfc74a881de5" 
+    default     = "ami-0d1b5a8c13042c939" 
 }
 variable "versioning" {
     type        = bool
